0b4a265ad3b92d7a871b7dc02befa9bc7fbc084a
msv69 <milo.salaveni@gemarship.ch>
2025-03-16 16:32:43 +0100

    Risoluzione conflitti

---
+++ src/_data/galleria.js
@@ -0,0 +1,13 @@
+onst Image = require("@11ty/eleventy-img");
+
+module.exports = async function() {
+  let images = await Image("./src/images/galleria/*.{jpg,png}", {
+    formats: ["webp"],
+    outputDir: "./_site/images/"
+  });
+
+  return Object.values(images).map(img => ({
+    url: img.webp[0].url,
+    desc: img.webp[0].sourcePath.split('/').pop().replace(/-/g, ' ')
+  }));
+};
