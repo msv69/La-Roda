70759e6b6a0a390f2f57a48ead89b3f5f557d740
msv69 <milo.salaveni@gemarship.ch>
2025-03-19 18:11:40 +0100

    Fix gestione immagini galleria

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy("src/admin");
-    
+
+
     return {
       dir: {
         input: "src",
--- src/_data/galleria.js
+++ src/_data/galleria.js
@@ -1,19 +1,21 @@
 const Image = require("@11ty/eleventy-img");
+const { glob } = require("glob"); // Aggiungi questa dipendenza
 
 module.exports = async function() {
-  try {
-    const images = await Image("./src/images/galleria/*.{jpg,png}", {
-      formats: ["webp"],
-      outputDir: "./_site/images/",
-      filenameFormat: (id, src, width, format) => {
-        const originalName = src.split('/').pop().split('.')[0];
-        return `${originalName}-${width}w.${format}`;
-      }
-    });
-
-    return Object.values(images).flatMap(format => format);
-  } catch (error) {
-    console.error("Errore nel processing immagini:", error);
-    return [];
-  }
+  // Usa glob per trovare i file correttamente
+  const imagePaths = await glob("./src/images/galleria/*.@(jpg|png)");
+  
+  return Promise.all(
+    imagePaths.map(async (path) => {
+      const stats = await Image(path, {
+        formats: ["webp"],
+        outputDir: "./_site/images/",
+        filenameFormat: (id, src, width, format) => {
+          const originalName = path.split('/').pop().split('.')[0];
+          return `${originalName}-${width}w.${format}`;
+        }
+      });
+      return stats.webp[0];
+    })
+  );
 };
