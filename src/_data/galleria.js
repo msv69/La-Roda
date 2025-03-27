39f978e8a71c901468be468ea69ff32df31b4870
msv69 <milo.salaveni@gemarship.ch>
2025-03-27 23:09:41 +0100

    Rimosso configurazione submodule errata

---
+++ src/_data/galleria.js
@@ -0,0 +1,7 @@
+const Image = require("@11ty/eleventy-img");
+
+module.exports = async () => {
+  const files = globSync("./src/images/galleria/*.{jpg,png}");
+  // Verifica che i percorsi siano corretti
+  console.log(files); 
+};
