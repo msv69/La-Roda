b18119ce30d1bbf526001fd22ec009d5f3bc487b
msv69 <milo.salaveni@gemarship.ch>
2025-03-31 21:07:48 +0200

    Force clean build

--- src/_data/galleria.js
+++ src/_data/galleria.js
@@ -1,7 +1,10 @@
-const Image = require("@11ty/eleventy-img");
+//const Image = require("@11ty/eleventy-img");
+
+const { eleventyImageTransformPlugin } = require('@11ty/eleventy-img');
 
 module.exports = async () => {
   const files = globSync("./src/images/galleria/*.{jpg,png}");
   // Verifica che i percorsi siano corretti
   console.log(files); 
 };
+
